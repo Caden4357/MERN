938623964283a405e441119d794379f9faaa7659
Caden Wilcox <wilcox4357@gmail.com>
2021-08-06 13:30:10 -0700

    prop it up done

---
+++ MERN Basics Week1/React/propitup/src/App.js
@@ -0,0 +1,18 @@
+import Main from './components/Main';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Main name="Wilcox, Caden" age="23" hairColor="Blonde"/>
+
+      <Main name="Smith, John" age="34" hairColor="Brown"/>
+
+      <Main name="Withers, Bill" age="58" hairColor="Black"/>
+
+      <Main name="Adams, Amy" age="38" hairColor="Red"/>
+    </div>
+  );
+}
+
+export default App;
